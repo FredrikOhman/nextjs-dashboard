0f5692ea7aa16e598c096091352b5f986d490bdc
FredrikOhman <fredrik_jo_o@hotmail.se>
2025-03-29 08:01:13 +0100

    feat: chapter 14 complete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,7 +74,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         `;
     } catch (error) {
         return {
-            message: 'Database Error: Failed to Create Invoice.',
+            message: `Database Error: Failed to Create Invoice. ${error}`,
         };
     }
 
@@ -82,13 +82,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -98,8 +110,9 @@ export async function updateInvoice(id: string, formData: FormData) {
             WHERE id = ${id}
         `;
     } catch (error) {
-        // We'll log the error to the console for now
-        console.error(error);
+        return {
+            message: `Database Error: Failed to Update Invoice. ${error}`,
+        };
     }
 
     revalidatePath('/dashboard/invoices');
@@ -107,7 +120,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
 
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
